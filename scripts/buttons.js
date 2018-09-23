5e32ba796f5327bdbdbb06ef0256194746ea2267
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-23 15:24:24 -0400

    added button setup

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,1 +1,17 @@
+class Button {
+  constructor(tempX, tempY, tempWidth, tempHeight, tempCallback){
+    this.x = tempX;
+    this.y = tempY;
+    this.width = tempWidth;
+    this.height = tempHeight;
+    this.callback = tempCallback;
+    this.error = tempError;
+  }
+  step(tempX, tempY){
+    if(tempX >= this.x && tempX <= (this.x+this.width) && tempY >= this.y && tempY <= (this.y+this.height)){
+      this.action();
+    }
+  }
+}
 
+export default Button;
