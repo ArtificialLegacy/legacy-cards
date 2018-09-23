ce923cb223e672ef13987f48cebb1599b608c80e
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-23 15:44:24 -0400

    Added events

--- client.js
+++ client.js
@@ -1,1 +1,27 @@
 import Button from './scripts/buttons.js';
+
+
+// Events
+function setup(){
+  
+}
+
+function preload(){
+  
+}
+
+function mouseClicked(){
+  
+}
+
+function draw(){
+  
+}
+
+function keyPressed(){
+  
+}
+
+function mouseClicked(){
+  
+}
