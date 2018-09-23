f15a00a70d11edff6b8989df40fb8fbf352a6c1b
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-23 18:54:38 -0400

    added menu handling

--- scripts/menuHandler.js
+++ scripts/menuHandler.js
@@ -1,5 +1,14 @@
 class MenuHandler {
-
+  contructor(tempMenus){
+    this.state = "main";
+    this.menus = tempMenus;
+  }
+  update(tempState){
+     this.state == tempState;
+  }
+  draw(){
+    this.menus[this.state]();
+  }
 }
 
 export default MenuHandler;
