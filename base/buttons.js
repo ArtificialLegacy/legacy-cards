9d9d6463b8d86a4a0435f04794a73bfd3212a166
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-23 19:24:37 -0400

    added scene support

--- base/buttons.js
+++ base/buttons.js
@@ -1,15 +1,18 @@
 class Button {
-  constructor(tempX, tempY, tempWidth, tempHeight, tempCallback){
+  constructor(tempX, tempY, tempWidth, tempHeight, tempScene, tempCallback){
     this.x = tempX;
     this.y = tempY;
     this.width = tempWidth;
     this.height = tempHeight;
     this.callback = tempCallback;
-    this.error = tempError;
+    this.scene = tempScene;
   }
-  step(tempX, tempY){
+  step(tempX, tempY, tempScene){
+    if(this.scene !== tempScene){
+      return;
+    }
     if(tempX >= this.x && tempX <= (this.x+this.width) && tempY >= this.y && tempY <= (this.y+this.height)){
-      this.action();
+      this.callback();
     }
   }
 }
