61bad3ebf7f953c4519f25eea811b4bcf39548ab
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-23 21:19:34 -0400

    added base code

--- base/packs.js
+++ base/packs.js
@@ -1,1 +1,10 @@
+class Packs {
+  constructor(tempName, tempDescription, tempCost, tempContent){
+    this.name = tempName;
+    this.description = tempDescription;
+    this.cost = tempCost;
+    this.content = tempContent;
+  }
+}
 
+export default Packs;
