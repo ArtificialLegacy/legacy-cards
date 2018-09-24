9a1c836fdabe5b9da1427b6588a287efba44035d
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-23 20:44:34 -0400

    Update cards.js

--- base/cards.js
+++ base/cards.js
@@ -1,10 +1,14 @@
 class Cards {
-  constructor(tempName, tempType, tempDescription, tempHealth=null, tempDamage=null){
+  constructor(tempName, tempType, tempDescription, tempHealth=null, tempDamage=null, tempAbility=()=>{}){
     this.name = tempName;
     this.type = tempType;
     this.description = tempDescription;
     this.health = tempHealth;
     this.damage = tempDamage;
+    this.ability = tempAbility;
+  }
+  use(){
+    this.ability();
   }
 }
 
