b1628f1263c13838bfcc4ce6bdefbe86ca0c1139
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-09-23 20:24:44 -0400

    added base system

--- base/cards.js
+++ base/cards.js
@@ -1,1 +1,11 @@
+class Cards {
+  constructor(tempName, tempType, tempDescription, tempHealth=null, tempDamage=null){
+    this.name = tempName;
+    this.type = tempType;
+    this.description = tempDescription;
+    this.health = tempHealth;
+    this.damage = tempDamage;
+  }
+}
 
+export default Cards;
